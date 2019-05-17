49a187efa60da60ba392a94ae5eaa0d954528f0d
Mark Strickland <mstrickland@globys.com>
2019-05-17 10:24:23 -0700

    Setup Firebase database, add Clients mock data

---
+++ src/constants/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCfJAv2USMp-lnA8Bko1JDc26iKJuKeZY4",
+  authDomain: "globysg3configurationtool.firebaseapp.com",
+  databaseURL: "https://globysg3configurationtool.firebaseio.com",
+  projectId: "globysg3configurationtool",
+  storageBucket: "globysg3configurationtool.appspot.com",
+  messagingSenderId: "737495831111"
+};
